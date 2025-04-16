092cf89ec2ae34f2ee52dd6e36e096bac6a45365
Alexander Tarasov <a.tevg@ya.ru>
2025-04-16 12:35:51 +0300

    version -> 0.3.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="yndx_disk",
-    version="0.3",
+    version="0.3.1",
     packages=find_packages(),
     install_requires=[
         "httpx", "aiofiles"
